e9a57a961b314069b84e23ffa4557893d4202a42
carstenbach <mail@carsten-bach.de>
2022-10-30 03:54:05 +0100

    WIP

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://figuren.theater
  * Text Domain:     ft-privacy
  * Domain Path:     /languages
- * Version:         1.0.0
+ * Version:         1.0.1
  *
  * @package         Figuren_Theater\Privacy
  */
